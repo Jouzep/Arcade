8a42083ba0b6223412399667c583f014351c3cf5
BRR steven <steven.dong@epitech.eu>
2023-03-16 16:19:53 +0100

    13:feat:implementing interface for game, graphics and object

---
+++ include/IObject.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade-Architecture
+** File description:
+** IObject
+*/
+
+#ifndef IOBJECT_HPP
+    #define IOBJECT_HPP
+
+namespace arcade {
+    class IObject {
+        virtual ~IObject() = default;
+    };
+}
+
+#endif
